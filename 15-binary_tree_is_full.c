7d0fd3d916127a8b4cca5b5b29f8c12ed874ecd8
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-15 17:33:11 +0200

    push

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Checks if a binary tree is full
+ * binary_tree_is_full - Checks if a binary tree is full
  * @tree: Pointer to the root node of the tree to check
  *
  * Return: ...
