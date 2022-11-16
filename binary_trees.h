14ce4ebb56815bb7c6a5f4005655b7d97176efcf
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-16 15:35:20 +0200

    push

--- binary_trees.h
+++ binary_trees.h
@@ -32,6 +32,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+typedef struct binary_tree_s bst_t;
+
+typedef struct binary_tree_s avl_t;
+
+typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -83,5 +89,13 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
 int binary_tree_is_bst(const binary_tree_t *tree);
+
+bst_t *bst_insert(bst_t **tree, int value);
+
+bst_t *array_to_bst(int *array, size_t size);
+
+bst_t *bst_search(const bst_t *tree, int value);
+
+bst_t *bst_remove(bst_t *root, int value);
 
 #endif /* _BINARY_TREES_H_ */
