e3f2006ea9c563d17ca27780e8304bb1c20b805b
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-17 11:50:44 +0200

    push

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,37 +8,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *c = tree, *temp;
-
 	if (!tree)
 		return;
-
-	while (1)
-	{
-		if (!c->left && !c->right)
-		{
-			if (c->parent)
-			{
-				temp = c->parent;
-				if (temp->left == c)
-					temp->left = NULL;
-				else
-					temp->right = NULL;
-				free(c);
-				c = temp;
-			}
-			else
-			{
-				free(c);
-				return;
-			}
-		}
-		else
-		{
-			if (c->left)
-				c = c->left;
-			else
-				c = c->right;
-		}
-	}
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
